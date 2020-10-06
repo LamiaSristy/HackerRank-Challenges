3faaf0cceaf553b27430082d8af225d2c0365098
lamiasristy <lamiasristy@gmail.com>
2020-10-06 11:59:50 +0200

    Added Project Euler #2: Even Fibonacci numbers solution .

--- eular2_even_fibonocci.rb
+++ eular2_even_fibonocci.rb
@@ -3,6 +3,24 @@
 t = gets.strip.to_i
 for a0 in (0..t-1)
     n = gets.strip.to_i
+    a = 0
+    b = 1
+    fib_num = a+b
+    sum = 0
+    while (fib_num < n)
+        if fib_num.even?
+              sum += fib_num
+        end      
+        a = b
+        b = fib_num
+        fib_num = a+b
+    end
+  puts sum
+end
+## This following versuion take less time 
+t = gets.strip.to_i
+for a0 in (0..t-1)
+    n = gets.strip.to_i
     a = 1
     b = 1
     fib_num = a+b
